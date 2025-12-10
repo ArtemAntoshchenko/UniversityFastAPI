2948b8c51572178158f01903d23e38b856594329
Антощенко Артем Олегович <279017030@ACADEMY.TOP>
2025-12-10 21:23:50 +0300

    first commit

---
+++ app/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from utils import *
+from typing import Optional
+import os
+
+script_dir=os.path.dirname(os.path.abspath(__file__))
+parent_dir=os.path.dirname(script_dir)
+path_to_json=os.path.join(parent_dir, 'students.json')
+
+app=FastAPI()
+
+@app.get('/students')
+def get_all_students():
+    # dict=json_to_dict_list(path_to_json)
+    # print(type(dict))
+    return json_to_dict_list(path_to_json)

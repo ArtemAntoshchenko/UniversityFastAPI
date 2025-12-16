1dfffea78ebc0eccb6c39b6ee6c42bb82fd79322
ArtemAntoshchenko <fgd19611@yndex.ru>
2025-12-16 15:27:52 +0300

    update main.py

--- app/main.py
+++ app/main.py
@@ -13,27 +13,44 @@ app=FastAPI()
 def home_page():
     return {'message':'Hello Python 47'}
 
-@app.get('/students')
-def get_all_students(course:Optional[int]=None):
+# @app.get('/students')
+# def get_all_students(course:Optional[int]=None):
+#     students=json_to_dict_list(path_to_json)
+#     if course is None:
+#         return students
+#     else:
+#         return_list=[]
+#         for student in students:
+#             if student['course']==course:
+#                 return_list.append(student)
+#         return return_list
+
+# @app.get('/students/{course}')
+# def get_students_in_course(course:int, major:Optional[str]=None, enrollment_year:Optional[int]=None):
+#     students=json_to_dict_list(path_to_json)
+#     filtered_students=[]
+#     for student in students:
+#         if student['course']==course:
+#             filtered_students.append(student)
+#     if major:
+#         filtered_students=[student for student in filtered_students if student['major'].lower()==major.lower()]
+#     if enrollment_year:
+#         filtered_students=[student for student in filtered_students if student['enrollment_year']==enrollment_year]
+#     return filtered_students
+
+@app.get('/students/{student_id}')
+def get_student_by_id1(student_id:int):
     students=json_to_dict_list(path_to_json)
-    if course is None:
+    for student in students:
+        if student['student_id']==student_id:
+            return student
+
+@app.get('/students')
+def get_student_by_id2(student_id:Optional[int]=None):
+    students=json_to_dict_list(path_to_json)
+    if student_id is None:
         return students
     else:
-        return_list=[]
         for student in students:
-            if student['course']==course:
-                return_list.append(student)
-        return return_list
-
-@app.get('/students/{course}')
-def get_students_in_course(course:int, major:Optional[str]=None, enrollment_year:Optional[int]=None):
-    students=json_to_dict_list(path_to_json)
-    filtered_students=[]
-    for student in students:
-        if student['course']==course:
-            filtered_students.append(student)
-    if major:
-        filtered_students=[student for student in filtered_students if student['major'].lower()==major.lower()]
-    if enrollment_year:
-        filtered_students=[student for student in filtered_students if student['enrollment_year']==enrollment_year]
-    return filtered_students
+             if student['student_id']==student_id:
+                 return student
